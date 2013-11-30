5533083c3bacfa54a5e0a9bccfe2ea50f3c02172
spmno <sunqingpeng@hotmail.com>
2013-11-30 22:30:47 +0800

    initialize ShenyangHeFan site. Come on.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShenyangHeFan::Application.config.secret_key_base = 'e286250823ca3504d546f21a5fea42354dac3cba561af0250c071d27d3f97bc6a81254bb90b71b12705d8a7034fdfd93327bbec43445301cdd82c6a008faef4c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShenyangHeFan::Application.config.session_store :cookie_store, key: '_ShenyangHeFan_session'
