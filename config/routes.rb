1cd5f1cc45a1bd9decceabfeb294b9e0807da586
spmno <sunqingpeng@hotmail.com>
2014-01-02 20:18:02 +0800

    1. add Chinese language support.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ShenyangHeFan::Application.routes.draw do
-  resources :he_fans
+  resources :hefans
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
